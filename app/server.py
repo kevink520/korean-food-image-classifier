0840f0def33a59c8d364b106ae9e8f97e37f9aa3
Kevin K <kevink520@Kevins-iMac.local>
2019-04-21 23:11:52 -0400

    Used load_learner

--- app/server.py
+++ app/server.py
@@ -45,10 +45,11 @@ async def download_file(url, dest):
 
 async def setup_learner():
     await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
-    data_bunch = ImageDataBunch.single_from_classes(path, classes,
-        ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
-    learn = cnn_learner(data_bunch, models.resnet50, pretrained=False)
-    learn.load(model_file_name)
+    #data_bunch = ImageDataBunch.single_from_classes(path, classes,
+    #    ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+    #learn = cnn_learner(data_bunch, models.resnet50, pretrained=False)
+    #learn.load(model_file_name)
+    learn = load_learner(path, model_file_name)    
     return learn
 
 loop = asyncio.get_event_loop()
